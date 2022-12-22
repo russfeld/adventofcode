20840b330167c4b6d0c6870f3bd427e4efb96ee0
Russell Feldhausen <russfeldh@gmail.com>
2022-12-21 23:29:59 -0600

    Day 16

--- 2022/Day16.py
+++ 2022/Day16.py
@@ -18,6 +18,59 @@ class Node:
     return str(self)
 
 
+def heuristic_search(paths, current, remain, output, visited, size, graph):
+  if len(visited) == size:
+    return output
+  next = {}
+  for step in paths[current]:
+    if step not in visited and (remain - paths[current][step] - 1) > 0:
+      value = (remain - paths[current][step] - 1) * graph[step].rate
+      next[step] = value
+  if len(next) == 0:
+    return output
+  ordered = sorted(next, key=next.get, reverse=True)
+  result = 0
+  for step in ordered:
+    visited.add(step)
+    # if len(visited) < 3:
+    #   print("{} : {}".format(visited, output + next[step]))
+    result = max(result, heuristic_search(paths, step, remain - paths[current][step] - 1, output + next[step], visited, size, graph))
+    visited.remove(step)
+  return result
+
+def super_heuristic_search(paths, you, you_remain, ele, ele_remain, output, visited, size, graph):
+  """
+  Not even sure I could explain this one if I tried
+  Basically tries to calculate next best step for you and elephant
+  and tries all possible combinations of them
+  It is kinda a "branch and bound" problem?
+  """
+  if len(visited) == size:
+    return output
+  you_next = {}
+  for step in paths[you]:
+    if step not in visited and (you_remain - paths[you][step] - 1) > 0:
+      value = (you_remain - paths[you][step] - 1) * graph[step].rate
+      you_next[step] = value
+  ele_next = {}
+  for step in paths[ele]:
+    if step not in visited and (ele_remain - paths[ele][step] - 1) > 0:
+      value = (ele_remain - paths[ele][step] - 1) * graph[step].rate
+      ele_next[step] = value
+  if len(you_next) + len(ele_next) == 0:
+    return output
+  result = 0
+  for you_step in you_next:
+    visited.add(you_step)
+    for ele_step in ele_next:
+      if ele_step == you_step:
+        continue
+      visited.add(ele_step)
+      result = max(result, super_heuristic_search(paths, you_step, (you_remain - paths[you][you_step] - 1), ele_step, (ele_remain - paths[ele][ele_step] - 1), output + you_next[you_step] + ele_next[ele_step], visited, size, graph))
+      visited.remove(ele_step)
+    visited.remove(you_step)
+  return result
+
 def find_path(graph, start, dest):
   # from day 12
   # BFS
@@ -44,7 +97,7 @@ def find_path(graph, start, dest):
 
 
 
-with open("test16.txt") as file:
+with open("input16.txt") as file:
   # dictionary of all nodes
   graph = {}
   # set of nodes with valves to be opened
@@ -72,24 +125,16 @@ with open("test16.txt") as file:
     paths["AA"][valve] = find_path(graph, "AA", valve)
   print(paths)
   # paths should now list the shortest paths between valves that can be opened
-  visited = set()
-  current = "AA"
-  remain = 30
-  output = 0
-  while visited != valves:
-    max_next = 0
-    next = ""
-    for step in paths[current]:
-      if step not in visited:
-        # greedy algorithm does not work :(
-        value = (remain - paths[current][step] - 1) * graph[step].rate
-        if value > max_next:
-          max_next = value
-          next = step
-    visited.add(next)
-    remain = remain - paths[current][next] - 1
-    output += max_next
-    current = next
-    print("{} : {} : {} : {}".format(current, remain, visited, output))
+
+  # let's try a heuristic approach
+  # ok, this ended up not being a heuristic but whatever
+  # this problem is fun?
+  output = heuristic_search(paths, "AA", 30, 0, set(), len(valves), graph)
+  print(output)
+
+  # Part 2 - WTF? Elephants?
+  output = super_heuristic_search(paths, "AA", 26, "AA", 26, 0, set(), len(valves), graph)
+  print(output)
+  
   
   
