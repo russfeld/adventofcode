c651858d334e662e8df6ee5f9fd5660855c4ee69
Russell Feldhausen <russfeldh@gmail.com>
2022-12-23 19:31:22 -0600

    Day 18 part 1

--- 2022/Day18.py
+++ 2022/Day18.py
@@ -2,46 +2,38 @@ import numpy as np
 
 
 arr = np.zeros((22, 22, 22), np.int8)
-
-def check(i, j, k):
-  output = 0
-  if i > 0 and arr[i-1][j][k] == 0:
-    output += 1
-  elif i == 0:
-    output += 1
-  if i < 21 and arr[i+1][j][k] == 0:
-    output += 1
-  elif i == 21:
-    output += 1
-  if j > 0 and arr[i][j-1][k] == 0:
-    output += 1
-  elif j == 0:
-    output += 1
-  if j < 21 and arr[i][j+1][k] == 0:
-    output += 1
-  elif j == 21:
-    output += 1
-  if k > 0 and arr[i][j][k-1] == 0:
-    output += 1
-  elif k == 0:
-    output =+ 1
-  if k < 21 and arr[i][j][k+1] == 0:
-    output += 1
-  elif k == 21:
-    output += 1
-  return output
+points = []
 
 with open("input18.txt") as file:
   for line in file:
     cube = [int(x) for x in line.split(",")]
     arr[cube[0]][cube[1]][cube[2]] = 1
+    points.append((cube[0], cube[1], cube[2]))
 
 count = 0
-for i in range(22):
-  for j in range(22):
-    for k in range(22):
-      if arr[i][j][k] == 1:
-        count += check(i, j, k)
-print(count)
+total_cover = 0
+
+for i in range(len(points)):
+  point = points[i]
+  covered = 0
+  if point[0] > 0 and arr[point[0]-1][point[1]][point[2]] == 1: 
+    covered += 1
+  if point[0] < 21 and arr[point[0]+1][point[1]][point[2]] == 1: 
+    covered += 1
+  if point[1] > 0 and arr[point[0]][point[1]-1][point[2]] == 1: 
+    covered += 1
+  if point[1] < 21 and arr[point[0]][point[1]+1][point[2]] == 1: 
+    covered += 1
+  if point[2] > 0 and arr[point[0]][point[1]][point[2]-1] == 1: 
+    covered += 1
+  if point[2] < 21 and arr[point[0]][point[1]][point[2]+1] == 1: 
+    covered += 1
+  total_cover += (covered)
+
+area = (6 * len(points)) - total_cover
+print(area)
+    
+    
+
 
 
