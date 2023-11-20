4d3fffd734d7f89f04d22345f68911e1aaababf9
Russell Feldhausen <russfeldh@gmail.com>
2023-11-20 14:50:12 -0600

    2021 part 3

--- 2021/src/three.ts
+++ 2021/src/three.ts
@@ -1,3 +1,5 @@
+import { listen } from "bun";
+
 export async function three_a() : Promise<number> {
   const file = Bun.file("inputs/three.txt");
   var counts: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -36,14 +38,41 @@ export async function three_a() : Promise<number> {
 
 export async function three_b() : Promise<number> {
   const file = Bun.file("inputs/three.txt");
+  var total: number = 0;
+  var values: string[] = (await file.text()).split("\n")
+  var values2: string[] = JSON.parse(JSON.stringify(values)); 
 
-
-  for (const line of (await file.text()).split("\n")) {
-    
-
+  for (const index of Array(12).keys()) {
+    const count: number = values.reduce((accumulator, value) => accumulator + parseInt(value[index]), 0);
+    if (count >= values.length / 2){
+      values = values.filter((element) => element[index] == "1");
+    } else {
+      values = values.filter((element) => element[index] == "0");
+    }
+    if (values.length == 1){
+      break;
+    }
   }
 
-  return 0;
+  //console.log(values)
+
+  for (const index of Array(12).keys()) {
+    const count: number = values2.reduce((accumulator, value) => accumulator + parseInt(value[index]), 0);
+    if (count >= values2.length / 2){
+      values2 = values2.filter((element) => element[index] == "0");
+    } else {
+      values2 = values2.filter((element) => element[index] == "1");
+    }
+    if(values2.length == 1){
+      break;
+    }
+  }
+
+  //console.log(values2)
+
+  const power = parseInt(values[0], 2) * parseInt(values2[0], 2);
+
+  return power
 }
 
 console.log(await three_a());
