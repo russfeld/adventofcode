b322a3a7a5825daae81626ed03ddf4f967fe8c80
Russell Feldhausen <russfeldh@gmail.com>
2023-12-01 10:32:23 -0600

    2023 1

--- 2023/src/a_one.py
+++ 2023/src/a_one.py
@@ -1,15 +1,55 @@
 def one_a() -> int:
-  with open("inputs/a_one.txt") as file:
-    for line in file:
-      print(line)
-  return 0
+    sum: int = 0
+    with open("inputs/a_one.txt") as file:
+        for line in file:
+            line = [x for x in line if x.isdigit()]
+            value = int(line[0] + line[-1])
+            sum += value
+    return sum
+
+
+numbers = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
 
 def one_b() -> int:
-  with open("inputs/a_one.txt") as file:
-    for line in file:
-      print(line)
-  return 0
+    sum: int = 0
+    with open("inputs/a_one.txt") as file:
+        for line in file:
+            line = line.strip()
+            vals = []
+            while len(vals) == 0:
+                for number, digit in numbers.items():
+                    if line.startswith(number):
+                        vals.append(digit)
+                        break
+                if line[0].isdigit():
+                    vals.append(line[0])
+                line = line[1:]
+            while len(vals) == 1 and len(line) > 0:
+                for number, digit in numbers.items():
+                    if line.endswith(number):
+                        vals.append(digit)
+                        break
+                if line[-1].isdigit():
+                    vals.append(line[-1])
+                line = line[:-1]
+            # print(vals)
+            value = int(vals[0] + vals[-1])
+            # print(value)
+            sum += value
+    return sum
+
 
 if __name__ == "__main__":
-  print(one_a())
-  print(one_b())
+    print(one_a())
+    print(one_b())
--- 2023/test_all.py
+++ 2023/test_all.py
@@ -3,7 +3,7 @@ sys.path.append("src")
 
 import a_one as one
 
-def test_one():
-  assert one.one_a() == 0
-  assert one.one_b() == 0
 
+def test_one():
+    assert one.one_a() == 54081
+    assert one.one_b() == 54649
