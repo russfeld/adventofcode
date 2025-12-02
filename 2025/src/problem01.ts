5b649290a7916e8ef594b9d249b022a42c514461
Russell Feldhausen <russfeldh@gmail.com>
2025-12-02 01:42:33 +0000

    Problem 1 Faster

--- 2025/src/problem01.ts
+++ 2025/src/problem01.ts
@@ -20,6 +20,33 @@ export function problem01a(inputfile: string): number {
   return count;
 }
 
+export function problem01afast(inputfile: string): number {
+  const input: string[] = [...Deno.readTextFileSync(inputfile)];
+  let dial: number = 50;
+  let count: number = 0;
+  let pos = 0;
+  let len = input.length
+  while(pos < len) {
+    const dir: string = input[pos++]
+    let turns: number = input[pos++].charCodeAt(0) - 48
+    while(pos < len && input[pos] != "\n" && input[pos] != "\r") {
+      turns = turns * 10 + input[pos++].charCodeAt(0) - 48
+    }
+    pos++
+    if (dir == "L") {
+      dial = mod((dial - turns), 100)
+    } else {
+      dial = mod((dial + turns), 100)
+    }
+    // console.log(`Turned ${dir} to ${dial}`)
+    if (dial == 0) {
+      count += 1;
+      // console.log(`Password ${count}`)
+    }
+  }
+  return count;
+}
+
 export function problem01b(inputfile: string): number {
   const input = Deno.readTextFileSync(inputfile);
   const array: string[] = input.split("\n");
@@ -65,9 +92,11 @@ function mod(a: number, b: number): number {
 if (import.meta.main) {
   console.log("Example Inputs");
   console.log(problem01a("inputs/01e.txt"));
+  console.log(problem01afast("inputs/01e.txt"));
   console.log(problem01b("inputs/01e.txt"));
   console.log("Real Inputs");
   console.log(problem01a("inputs/01.txt"));
+  console.log(problem01afast("inputs/01.txt"));
   console.log(problem01b("inputs/01.txt"));
 }
 
@@ -77,4 +106,8 @@ Deno.bench("problem01a", () => {
 
 Deno.bench("problem01b", () => {
   problem01b("inputs/01.txt");
+});
+
+Deno.bench("problem01afast", () => {
+  problem01afast("inputs/01.txt");
 });
